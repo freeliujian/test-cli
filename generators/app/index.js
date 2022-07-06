4911dcfe9f6837aece0bd9e0e9cba0dcd6f6cbe2
littleforest <670177821@qq.com>
2022-07-06 18:00:46 +0800

    新增yeoman

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,14 +1,26 @@
-const Generator = require('yeoman-generator')
-const BasicGenerator = require('../../BaseGenerator')
-
+const Generator = require("yeoman-generator");
+const BasicGenerator = require("../../BaseGenerator");
 
 module.exports = class Generator extends BasicGenerator {
-
-  prompting(){
-    
+  prompting() {
+    this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "你的名字",
+        default: "张三",
+      },
+      {
+        type: "input",
+        name: "age",
+        message: "请输入你的年龄",
+      },
+    ]);
   }
 
-  default(){
-    console.log('defalut');
+  
+  writings (){
+    this.writing();
   }
-}
+  
+};
